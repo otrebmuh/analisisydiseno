c69c423d21c2710541691a8776377f619bf36d3d
Humberto Cervantes <hcm@xanum.uam.mx>
2018-03-06 15:00:26 -0600

    Commit inicial

---
+++ EjemploGit/src/mx/uam/ayd/Aplicacion.java
@@ -0,0 +1,15 @@
+package mx.uam.ayd;
+
+public class Aplicacion {
+
+	public static void main(String[] args) {
+		Aplicacion ap = new Aplicacion();
+		ap.inicia();
+
+	}
+	
+	public void inicia() {
+		System.out.println("Hola");
+	}
+
+}
